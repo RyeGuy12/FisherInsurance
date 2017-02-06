807b7c512dfaaa4d9d508ee017c463c7860cd435
RyeGuy12 <hawkinshodgson.1@buckeyemail.osu.edu>
2017-02-06 16:55:17 -0500

    Maintained the naming conventions for CustomerCareController Methods but implemented the desired routes

--- Controllers/CustomerCareController.cs
+++ Controllers/CustomerCareController.cs
@@ -6,10 +6,20 @@ public class CustomerCareController : Controller
     {        
         return Ok("This is the index of the CustomerCareController");    
     }
-[RouteAttribute("claims")]
+    [RouteAttribute("claim")]
         public IActionResult Quote()    
     {        
         return Ok("This is the index of the CustomerCareQuoteController");    
+    }
+    [RouteAttribute("fileclaim")]
+    public IActionResult NewClaim()
+    {
+        return Ok("This abides to the naming convention while maintaing the desired route.");
+    }
+    [RouteAttribute("claimstatus")]
+    public IActionResult ClaimHistory()
+    {
+        return Ok("The naming convention is ClaimHistory but the route is myclaims");
     }
 
 }
